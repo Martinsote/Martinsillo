b33962b2a22c7151ed06ce28f1979cf989c8661a
Martin Najera <najeramartin0@gmail.com>
2017-11-09 15:55:57 -0600

    first commit

---
+++ web/index.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>League of Legends</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="album.css" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+    <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 py-4">
+                    <h4 class="text-white">Acerca de...</h4>
+                    <p class="text-muted">Aqui esta la super paginota del martinsote
+                    </p>
+                </div>
+                <div class="col-sm-4 py-4">
+                    <h4 class="text-white">Contact</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.facebook.com/MartinNajeraGlz" class="text-white">Agregame a Facebook</a></li>
+                        <li><a href="https://youtu.be/9uaL2yFtoVo" class="text-white">Escucha esta sexy canción</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark">
+        <div class="container d-flex justify-content-between">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+</header>
+
+<main role="main">
+
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1 class="jumbotron-heading">Galeria de imagenes de League of Legends</h1>
+            <p class="lead text-muted">Una galeria dedicada a mis campeones favoritos</p>
+            <p>
+            </p>
+        </div>
+    </section>
+
+    <div class="album text-muted">
+        <div class="container">
+
+            <div class="row">
+                <div class="card">
+                    <img data-src="holder.js/100px280?theme=thumb" alt="Katarina en tiempos de guerra">
+                    <p class="card-text">Uno de mis campeones favoritos con uno de los skins que igual me encantan.</p>
+                </div>
+                <div class="card">
+                    <img src="images/kata.jpg" alt="Card image cap">
+                    <p class="card-text">Darius Rey de las Clavadas</p>
+                </div>
+                <div class="card">
+                    <img src="images/Darius.jpg" alt="Card image cap">
+                    <p class="card-text">Mi campeon favorito <3</p>
+                </div>
+
+                <div class="card">
+                    <img src="images/Jax.jpg" alt="Card image cap">
+                    <p class="card-text">Es uno de los campeones que mas tiempo les dedique y logré dominar.</p>
+                </div>
+                <div class="card">
+                    <img src="images/Teemo.jpg" alt="Card image cap">
+                    <p class="card-text">El mismisimo demonio, el campeon mas desbalanceado de todos, y con muchisimo hate y love</p>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</main>
+
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Volver al inicio</a>
+        </p>
+
+        <p>Le gustó? califiqueme y pongame un sexy 10 :D</p>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="../../../../assets/js/vendor/holder.min.js"></script>
+<script>
+    Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+    });
+</script>
+</body>
+</html>
